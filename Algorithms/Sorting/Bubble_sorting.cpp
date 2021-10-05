f373d54f88ebf12308d57fcedd4ba4d84d3c436e
vinaybaliyan11 <87595477+vinaybaliyan11@users.noreply.github.com>
2021-10-05 16:15:22 +0530

    Update Bubble_sorting.cpp

--- Algorithms/Sorting/Bubble_sorting.cpp
+++ Algorithms/Sorting/Bubble_sorting.cpp
@@ -1,30 +1,31 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void swapping(int &a, int &b) {      //swap the content of a and b
-   int temp;
-   temp = a;
-   a = b;
-   b = temp;
-}
 void display(int *array, int size) {
    for(int i = 0; i<size; i++)
       cout << array[i] << " ";
    cout << endl;
 }
-void bubbleSort(int *array, int size) {
-   for(int i = 0; i<size; i++) {
-      int swaps = 0;         //flag to detect any swap is there or not
-      for(int j = 0; j<size-i-1; j++) {
-         if(array[j] > array[j+1]) {       //when the current item is bigger than next
-            swapping(array[j], array[j+1]);
-            swaps = 1;    //set swap flag
-         }
-      }
-      if(!swaps)
-         break;       // No swap in this pass, so array is sorted
+void bubbleSort(int *array, int n)
+{
+   bool swapped; // Using this, the algo can be optimized by stopping the execution if inner loop didn't cause any swap 
+   for (size_t i = 0; i < n-1; i++)
+   {
+     swapped = false;
+     for (size_t j = 0; j < n-i-1; j++)
+     {
+        if (arr[j] > arr[j+1])
+        {
+           swap(&arr[j], &arr[j+1]);
+           swapped = true;
+        }
+     }
+ 
+     // if no two elements were swapped by inner loop, then break
+     if (swapped == false)
+        break;
    }
-}
-int main() {
+   
+}int main() {
    int n;
    cout << "Enter the number of elements: ";
    cin >> n;
